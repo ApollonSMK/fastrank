dd92b13faf0c7ef07c9030e06036cc78a54621e7
ApollonSMK Silva <microgramxx@gmail.com>
2025-06-26 15:22:26 +0000

    ainda nao da, deve ser porque no login que voce criou nao da conectado a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDt6uEYbYk0VZE7CY_kMIgW3H3daxp7F18",
   authDomain: "fastrack-f6f0a.firebaseapp.com",
   projectId: "fastrack-f6f0a",
-  storageBucket: "fastrack-f6f0a.appspot.com",
+  storageBucket: "fastrack-f6f0a.firebasestorage.app",
   messagingSenderId: "282991338208",
   appId: "1:282991338208:web:f7ed985f99ca70068a2685",
   measurementId: "G-CB6MZYQCM8"
